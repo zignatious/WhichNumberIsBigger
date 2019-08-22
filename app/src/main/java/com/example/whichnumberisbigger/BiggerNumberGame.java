f60a0a722bf1bd204cd74008c13bbd33363359e9
zignatious <heysigmund@gmail.com>
2019-08-22 13:56:16 -0700

    created the BiggerNumberGame POJO Generates random numbers and checks answers

---
+++ app/src/main/java/com/example/whichnumberisbigger/BiggerNumberGame.java
@@ -0,0 +1,75 @@
+package com.example.whichnumberisbigger;
+
+public class BiggerNumberGame {
+    private int number1;
+    private int number2;
+    private int score;
+    private int lowerLimit;
+    private int upperLimit;
+
+    public BiggerNumberGame(int number1, int upperLimit) {
+        this.lowerLimit = lowerLimit;
+        this.upperLimit = upperLimit;
+    }
+
+    public void generateRandomNumbers() {
+        number1 = (int)((upperLimit - lowerLimit + 1) * Math.random() + lowerLimit);
+        number2 = (int)((upperLimit - lowerLimit + 1) * Math.random() + lowerLimit);
+        while (number1 == number2) {
+            number2 = (int)((upperLimit - lowerLimit + 1) * Math.random());
+            }
+        }
+    //generate a number between the upper and lower limits inclusive
+        //store that in number1
+        //generate another number and make sure that it's different
+        //store that in number2
+
+
+//    public String checkAnswer(int answer) {
+   //determine if the answer is right
+        //based on the values of number 1 and number 2
+        // update the score accordingly
+      //return a relevant message
+//  }
+
+
+    public int getNumber2() {
+        return number2;
+    }
+
+    public void setNumber2(int number2) {
+        this.number2 = number2;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public int getLowerLimit() {
+        return lowerLimit;
+    }
+
+    public void setLowerLimit(int lowerLimit) {
+        this.lowerLimit = lowerLimit;
+    }
+
+    public int getUpperLimit() {
+        return upperLimit;
+    }
+
+    public void setUpperLimit(int upperLimit) {
+        this.upperLimit = upperLimit;
+    }
+
+    public int getNumber1() {
+        return number1;
+    }
+
+    public void setNumber1(int number1) {
+        this.number1 = number1;
+    }
+}
